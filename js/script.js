45fff7e5a34cee4a3fffa3fd7f8ce851651e99b2
Bruno Leite <brunomarshall1996@gmail.com>
2022-11-19 23:31:37 -0300

    ativando produto na pg de orcamento com js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// ativar links do menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -10,3 +11,17 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+//ativar item no orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+    console.log(elemento);
+  }
+}
+
+parametros.forEach(ativarProduto);
