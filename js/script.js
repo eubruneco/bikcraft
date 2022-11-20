3898e5d6ecdf018b46cc92b94fb8f9fc4b1c1294
Bruno Leite <brunomarshall1996@gmail.com>
2022-11-20 00:32:39 -0300

    js na galeria de img com bikes

--- js/script.js
+++ js/script.js
@@ -45,3 +45,23 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// galeria de bikes
+
+const galeria = document.querySelectorAll(".bicicleta-img img");
+
+const galeriaContainer = document.querySelector(".bicicleta-img");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
